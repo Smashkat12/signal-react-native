3ed0a491f6c17726c34c634db495bc5549313763
Smashkat12 <smashkat12@gmail.com>
2021-01-28 13:48:25 +0200

    set up project with basic react navigation

---
+++ firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAXQkjFqmf5c_FkKI6vJE8ammnx_FA5ti4",
+  authDomain: "signal-clone-smash.firebaseapp.com",
+  projectId: "signal-clone-smash",
+  storageBucket: "signal-clone-smash.appspot.com",
+  messagingSenderId: "981116111282",
+  appId: "1:981116111282:web:6b9da15428df13bb44bcab",
+};
